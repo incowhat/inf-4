3cdc2639dd7ebd82d8592ac3c95b38c72cad1c71
incowhat <105190724+incowhat@users.noreply.github.com>
2022-09-19 11:20:03 +0200

    added missing string

--- PR 1.6 Vzdialenosť dvoch bodov.py
+++ PR 1.6 Vzdialenosť dvoch bodov.py
@@ -3,4 +3,4 @@ from math import sqrt
 ax, ay, bx, by = list(map(int, [input(f'{i} = ') for i in ['Ax', 'Ay', 'Bx', 'By']]))
 
 vzd = round(sqrt((bx-ax)**2 + (by-ay)**2), 2)
-print(f'Vzdialenost bodov A[{ax}, {ay}] a A[{bx}, {by}] je {vzd})
+print(f'Vzdialenost bodov A[{ax}, {ay}] a A[{bx}, {by}] je {vzd}')
