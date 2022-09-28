9d49640dc7e108d160c5956009f5fca1c0069180
incowhat <you@example.com>
2022-09-28 07:23:19 +0200

    pismenka update

--- PR 2.5 Písmenká.py
+++ PR 2.5 Písmenká.py
@@ -5,18 +5,22 @@ total = int(input('pocet pismen: '))
 
 canvas = tkinter.Canvas(width=400, height = 400)
 canvas.pack(fill="both", expand=True)
-width, height = 400, 400
+width, height = 0, 0
 
 while True:
     if width != canvas.winfo_width() or height != canvas.winfo_height():
+        width, height = canvas.winfo_width(), canvas.winfo_height()
+        canvas.delete("all")
+
         for i in range(total):
             x, y = randint(0, width), randint(0, height)
-            print(x, y)
+            # print(x, y)
             if x < width/2:
                 text, color = ('A', '#ff2200') if (y < height/2) else ('B', '#0022ff')
             else:
                 text, color = ('C', '#00ff22') if (y < height/2) else ('D', '#ffcc00')
-            print(text, color)
-            canvas.create_text(x, y, text=text)
+            # print(text, color)
+            canvas.create_text(x, y, text=text, fill=color)
             canvas.update()
-    width, height = canvas.winfo_width(), canvas.winfo_height()
+
+    canvas.update()
