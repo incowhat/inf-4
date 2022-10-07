08ef21bac178401aa1b0cdd7ae3b7b9b02c6d35b
incowhat <oliver.fric+git@gmail.com>
2022-10-07 10:34:24 +0200

    uloha 5.3

--- PR 4.7 Vybodkuj terč.py
+++ PR 4.7 Vybodkuj terč.py
@@ -7,7 +7,7 @@ canvas.pack(fill="both")
 r = 5
 nevydalo = 0
 
-for i in range(10000):
+for i in range(5000):
     x, y = randint(0, 300), randint(0, 300)
     kruh = lambda: (x-150)**2 + (y-150)**2 
 
@@ -20,9 +20,8 @@ for i in range(10000):
         color = 'black'
 
     canvas.create_oval(x-r, y-r, x+r, y+r, fill=color)
-    # canvas.update()
+    canvas.update()
 
 canvas.create_text(275, 280, text=nevydalo, fill='#ee2200', font='30')
 print(nevydalo)
-print(nevydalo/10000)
 tkinter.mainloop()
---
+++ PR 5.3 Sústredné štvorce – farebný prechod.py
@@ -0,0 +1,34 @@
+import tkinter
+from random import randint
+
+a = int(input('zadaj cislo strany: '))
+rgb = [0, 0, 255]
+color = lambda: f"#{rgb[0]:02x}{rgb[1]:02x}{rgb[2]:02x}"
+
+canvas = tkinter.Canvas(width=a, height = a)
+canvas.pack(fill="both")
+
+for i in range(1, (a>>1)+1):
+    canvas.create_rectangle(i, i, a, a, outline=color())
+    a -= 1
+    
+    for n in range(3):
+        if rgb[n] == 255:
+            if rgb[n-2] < 255:
+                rgb[n-2] += 1
+            if rgb[n-1] != 0:
+                rgb[n-1] -= 1
+            elif rgb[n-2] == 255:
+                rgb[n] -= 1
+                
+                
+
+
+
+    print(color())
+    canvas.update()
+    canvas.after(10)
+
+
+
+tkinter.mainloop()
