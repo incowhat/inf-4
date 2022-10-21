5e8febaad89126b4900e8ef8fbb3e9111d15513b
incowhat <oliver.fric+git@gmail.com>
2022-10-21 10:31:01 +0200

    generator color in siet

--- PR 5.2 Sieť.py
+++ PR 5.2 Sieť.py
@@ -1,4 +1,4 @@
-import tkinter
+import tkinter, color
 
 a = int(input('zadaj cislo strany: '))
 apol = a>>1
@@ -6,12 +6,26 @@ apol = a>>1
 canvas = tkinter.Canvas(width=a, height = a)
 canvas.pack(fill="both")
 
+def color(step=1, rgb=[255, 0, 0]):
+    while True:
+        for _ in range(step):
+            for n in range(3):
+                if rgb[n] == 255:
+                    if rgb[n-1] != 0:
+                        rgb[n-1] -= 1
+                    elif rgb[n-2] < 255:
+                        rgb[n-2] += 1
+                    else:
+                        rgb[n] -= 1
+            
+        yield f"#{rgb[0]:02x}{rgb[1]:02x}{rgb[2]:02x}"
+
 for c in range(0, apol+4, 5):
-    canvas.create_line(c, apol, apol, c+apol)
-    canvas.create_line(c, apol, apol, -c+apol)
-    canvas.create_line(-c+a, apol, apol, c+apol)
-    canvas.create_line(-c+a, apol, apol, -c+apol)
+    canvas.create_line(c, apol, apol, c+apol, fill=next(color(6)))
+    canvas.create_line(c, apol, apol, -c+apol, fill=next(color(6)))
+    canvas.create_line(-c+a, apol, apol, c+apol, fill=next(color(6)))
+    canvas.create_line(-c+a, apol, apol, -c+apol, fill=next(color(6)))
     canvas.update()
-    canvas.after(5)
+    canvas.after(50)
 
 tkinter.mainloop()
--- test.py
+++ test.py
@@ -1,2 +1,6 @@
-print(a:=5)
-print(type(a))
+listl = [i for i in range(10)]
+generator = (i for i in range(10))
+
+print(type(listl))
+print(type(generator))
+print(type(range(2)))
