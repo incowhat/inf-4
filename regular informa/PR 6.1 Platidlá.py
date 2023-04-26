acc2e28b92571b32bd43fdb78291700d00893262
incowhat <you@example.com>
2023-04-26 09:31:17 +0200

    platidla

--- regular informa/PR 6.1 Platidlá.py
+++ regular informa/PR 6.1 Platidlá.py
@@ -5,15 +5,18 @@ def zistiKelo(bankovka, suma):
     if pocet != 0:
         print(('bankovky' if bankovka >= 5 else 'mince   '), f'{bankovka:>3}€: {pocet:>3} ks.')
     
-    if 0 < suma:
-        if   bankovka == 500: zistiKelo(200, suma)
-        elif bankovka == 200: zistiKelo(100, suma)
-        elif bankovka == 100: zistiKelo( 50, suma)
-        elif bankovka ==  50: zistiKelo( 20, suma)
-        elif bankovka ==  20: zistiKelo( 10, suma)
-        elif bankovka ==  10: zistiKelo(  5, suma)
-        elif bankovka ==   5: zistiKelo(  2, suma)
-        else: zistiKelo(1, suma)
+    if suma == 0:
+        return
+
+    match bankovka:
+        case 500: zistiKelo(200, suma)
+        case 200: zistiKelo(100, suma)
+        case 100: zistiKelo(50, suma)
+        case 50: zistiKelo(20, suma)
+        case 20: zistiKelo(10, suma)
+        case 10: zistiKelo(5, suma)
+        case 5: zistiKelo(2, suma)
+        case _: zistiKelo(1, suma)
 
 suma = int(input('zadaj sumu: '))
 print(f'Suma {suma}€ sa da vyskladat z tychto eurobankoviek a eurominci:')
